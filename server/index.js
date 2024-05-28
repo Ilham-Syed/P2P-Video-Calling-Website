0c0821b9c9c7af9528e05331e41ba1d2e9570456
Ilham-Syed <ilhamsyed1310@gmail.com>
2024-05-29 02:13:45 +0530

    First Server side commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import {Server} from 'socket.io';
+
+const io=new Server(8000,{
+    cors:true,
+});
+
+const emailToSocketIdMap= new Map();
+const socketIdToEmailMap= new Map();
+io.on('connection',socket=>{
+    console.log("New socket connection established from", socket.id);
+    socket.on('room:join', data=>{
+        const {email,room}=data;
+        console.log({email,room});
+        emailToSocketIdMap.set(email,socket.id);
+        socketIdToEmailMap.set(socket.id,email);
+        io.to(socket.id).emit('room:join', data);
+    })
+})
